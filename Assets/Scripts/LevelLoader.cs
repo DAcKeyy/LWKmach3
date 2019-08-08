42d92aa6e0cef2d8c92383c36b83f6dae7650f89
Alexandr.D <42407914+Memerony@users.noreply.github.com>
2019-08-08 18:02:43 +0300

    Добавил меню
    Да

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] GameObject loadingPanel = null;
+    [SerializeField] Image LoadIndicator;
+
+    public void LoadLevel(string Scene)
+    {
+        StartCoroutine(LoadAsynchronously(Scene));
+    }
+
+    IEnumerator LoadAsynchronously (string Scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(Scene);
+
+        loadingPanel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            LoadIndicator.fillAmount = progress;
+
+            yield return null;
+        }
+    }
+}
