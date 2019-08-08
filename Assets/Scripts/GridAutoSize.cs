0daa0b77f033b43b80c700a9fef0fd0ae3064427
Alexandr.D <42407914+Memerony@users.noreply.github.com>
2019-08-08 21:29:24 +0300

    Купоны 1

---
+++ Assets/Scripts/GridAutoSize.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridAutoSize : MonoBehaviour
+{
+    [SerializeField] GameObject Conatainer;
+
+    void Update()
+    {
+        float wight = Conatainer.GetComponent<RectTransform>().rect.width;
+        Vector2 newSize = new Vector2((wight / 3) - 10, (wight / 4) - 10);
+        Conatainer.GetComponent<GridLayoutGroup>().cellSize = newSize;
+    }
+}
