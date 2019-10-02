749121ae881c3bc28d47b42d72b4208488e76781
Alexandr.D <42407914+Memerony@users.noreply.github.com>
2019-10-02 22:00:09 +0300

    Переделываю колесо под ДОТвины

---
+++ Assets/Scripts/Roulette/RouletteManagaer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class RouletteManagaer : MonoBehaviour
+{
+    [SerializeField] GameObject WinGiftPanel = null;
+    [SerializeField] GameObject WeelPanel = null;
+
+    private Vector2 StartPosition;
+
+    private void Start()
+    {
+        StartPosition = WeelPanel.transform.position;
+        Appear();
+    }
+
+    private void OnEnable()
+    {
+        RouletteMover.EndRotate += Disappear;
+    }
+    private void OnDisable()
+    {
+        RouletteMover.EndRotate -= Disappear;
+    }
+
+    #region Appear
+    public void Appear()
+    {
+        WeelPanel.transform.DOMove(Vector2.zero, 0.25F);
+    }
+
+    #endregion
+    #region Disappear
+    private void Disappear()
+    {
+        WeelPanel.transform.DOMove(StartPosition, 0.25F);
+    }
+
+    #endregion
+
+    private void ActivateWinGift()
+    {
+
+    }
+
+    public void SetActive()
+    {
+
+    }
+}
