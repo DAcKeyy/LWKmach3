915b3c68eb06324d7b5f2da098d724109dd3e031
shaman1979 <upgamestudio@gmail.com>
2020-02-29 15:52:18 +0300

    Реализовал паттерн синглтон

---
+++ Assets/Scripts/System/Sington.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LWR.System
+{
+    /// <summary>
+    /// Реализация паттерна синглтон
+    /// </summary>
+    /// <typeparam name="T">Класс который должен быть в одном экземпляре</typeparam>
+    public abstract class Sington<T> where T : new()
+    {
+        public static T Instatiate { get => instatiate; }
+        private static T instatiate = new T();
+    }
+}
