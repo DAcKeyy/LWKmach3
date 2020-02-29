20641e83a923a687aa650305633d178805a0fede
shaman1979 <upgamestudio@gmail.com>
2020-02-29 16:14:27 +0300

    Изменил namespace

--- Assets/Scripts/System/IPublisher.cs
+++ Assets/Scripts/System/IPublisher.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LWR.System
+namespace LWT.System
 {
     public interface IPublisher
     {
--- Assets/Scripts/System/Sington.cs
+++ Assets/Scripts/System/Sington.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LWR.System
+namespace LWT.System
 {
     /// <summary>
     /// Реализация паттерна синглтон
