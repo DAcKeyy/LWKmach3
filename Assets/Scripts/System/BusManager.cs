bb236b6530adb5c4d501c901d5f4e45184482aaf
shaman1979 <upgamestudio@gmail.com>
2020-02-29 16:01:01 +0300

    Добавил summery

--- Assets/Scripts/System/BusManager.cs
+++ Assets/Scripts/System/BusManager.cs
@@ -9,6 +9,11 @@ namespace LWR.System
     {
         private Dictionary<Type, Action<object>> subscriders = new Dictionary<Type, Action<object>>();
 
+        /// <summary>
+        /// Подпись на рассылку сообщения  
+        /// </summary>
+        /// <typeparam name="T">Тип сообщения</typeparam>
+        /// <param name="handler">Обработчик</param>
         public void Subscribe<T>(Action<T> handler)
         {
             if (subscriders == null)
@@ -24,11 +29,22 @@ namespace LWR.System
             subscriders[typeof(T)] =
                 (Action<object>)Delegate.Combine(subscriders[typeof(T)], (Action<object>)(o => { handler((T)o); }));
         }
+
+        /// <summary>
+        /// Отпись от сообщения 
+        /// </summary>
+        /// <typeparam name="T">Тип сообщения</typeparam>
+        /// <param name="handler">Обработчик</param>
         public void UnSubscribe<T>(Action<T> handler)
         {
-
+            //Реализую позже
         }
 
+        /// <summary>
+        /// Публикация сообщения
+        /// </summary>
+        /// <typeparam name="T">Тип сообщения</typeparam>
+        /// <param name="message">Сообщение</param>
         public void Publish<T>(T message)
         {
             if (subscriders.ContainsKey(typeof(T)))
