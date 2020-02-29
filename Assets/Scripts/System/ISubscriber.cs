6a5d106ae8284ddc21bf11100ca362564835938a
shaman1979 <upgamestudio@gmail.com>
2020-02-29 15:54:29 +0300

    Интерфейсы для работы с шиной(если мы перейдем на Zenject)

---
+++ Assets/Scripts/System/ISubscriber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LWR.System
+{
+    public interface ISubscriber
+    {
+        void Subscribe<T>(Action<T> handler);
+        void UnSubscribe<T>(Action<T> handler);
+    }
+}
