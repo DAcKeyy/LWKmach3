aa5f90688fa082d74fbae2a37d634aa2fe74ea36
shaman1979 <upgamestudio@gmail.com>
2020-02-29 15:54:47 +0300

    Шина сообщений

---
+++ Assets/Scripts/System/BusManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LWR.System
+{
+    public class MessageBusManager : Sington<MessageBusManager>, IPublisher, ISubscriber
+    {
+        private Dictionary<Type, Action<object>> subscriders = new Dictionary<Type, Action<object>>();
+
+        public void Subscribe<T>(Action<T> handler)
+        {
+            if (subscriders == null)
+            {
+                return;
+            }
+
+            if (!subscriders.ContainsKey(typeof(T)))
+            {
+                subscriders[typeof(T)] = o => { };
+            }
+
+            subscriders[typeof(T)] =
+                (Action<object>)Delegate.Combine(subscriders[typeof(T)], (Action<object>)(o => { handler((T)o); }));
+        }
+        public void UnSubscribe<T>(Action<T> handler)
+        {
+
+        }
+
+        public void Publish<T>(T message)
+        {
+            if (subscriders.ContainsKey(typeof(T)))
+            {
+                subscriders[typeof(T)].Invoke(message);
+            }
+        }
+    }
+}
