c6fab2c1fe658c3c51eff07adb5dcb413708c1e8
shaman1979 <upgamestudio@gmail.com>
2020-03-04 20:07:31 +0300

    Добавил обработчик ввода

---
+++ Assets/Scripts/System/InputHandles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace LWT.System
+{
+    /// <summary>
+    /// Обработчик UI элементов
+    /// </summary>
+    public class InputHandles : IInitializable
+    {
+        public event Action StartGameClick;
+        public event Action CouponClick;
+
+        private readonly UIElements elements;
+
+        public InputHandles(UIElements elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Initialize()
+        {
+            elements.StartGame.onClick.AddListener(() => StartGameClick?.Invoke());
+            elements.Coupon.onClick.AddListener(() => CouponClick?.Invoke());
+        }
+
+        [Serializable]
+        public class UIElements
+        {
+            public Button StartGame;
+            public Button Coupon;
+        }
+    }
+}
