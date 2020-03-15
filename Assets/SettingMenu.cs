a86d980ac1b024542d60d05a5227cb3f04dcdd84
shaman1979 <upgamestudio@gmail.com>
2020-03-15 20:24:29 +0300

    Окно настроек игры

---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Zenject;
+using LWT.System;
+
+namespace LWT.UI.Menu
+{
+    public class SettingMenu : MonoBehaviour
+    {
+        [Inject]
+        private InputHandles inputHandles;
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Show(float time)
+        {
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Hide(float time)
+        {
+        }
+
+        private void Start()
+        {
+            inputHandles.BackToMenu +=Hide;
+            inputHandles.SettingClick += Show;
+        }
+    }
+}
