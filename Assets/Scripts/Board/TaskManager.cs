68adc645917aaf782087e285e91cd6df33505c9e
Memerony <lewaLOC@gmail.com>
2020-07-05 19:32:45 +0300

    исправлен ещё один баг с опредлеление задач(подключил рандом)

--- Assets/Scripts/Board/TaskManager.cs
+++ Assets/Scripts/Board/TaskManager.cs
@@ -36,7 +36,7 @@ public class TaskManager : MonoBehaviour
         {
            if (piece.matchValue == tasks[0].transform.GetChild(0).GetComponent<Piece>().matchValue)
            {
-               Debug.Log("Task complete!");
+               //Debug.Log("Task complete!");
                TaskComplete?.Invoke();
                UpdateTask();
                break;
@@ -47,27 +47,35 @@ public class TaskManager : MonoBehaviour
 
     private void UpdateTask()
     {
+        
         Transform task0 = tasks[0].transform.GetChild(0).transform;
-        tasks[0].transform.GetChild(0).SetParent(null);
+        tasks[0].transform.GetChild(0).SetParent(null);//Удалить дитё задания
         task0.DOPunchScale(new Vector3(2f, 2f, 0f), 1f, 1, 1f);
-        task0.DOMoveX(-board.width, 1f).OnComplete(() => MoveTaskToEnd(task0));
-        for (int i = 1; i < tasks.Count; i++)
+        task0.DOMoveX(-board.width, 1f).OnComplete(() => MoveTaskToEnd(task0));//убрать превую фишку задания нахуй с поля таска
+
+        for (int i = 1; i < tasks.Count; i++) //передвинуть фишечки кроме первой
         {
             tasks[i].transform.GetChild(0).DOMoveX(tasks[i - 1].transform.position.x, 0.5f);
             tasks[i].transform.GetChild(0).SetParent(tasks[i - 1].transform);
         }
     }
 
-    private void MoveTaskToEnd(Transform task)
+    private void MoveTaskToEnd(Transform task)//вернуть превую фишку задания в конец таска на место 2го таска
     {
-        task.SetParent(tasks[2].transform);
-        task.transform.position = new Vector2(board.width, task.transform.position.y);
-        task.DOMoveX(tasks[2].transform.position.x, 0.3f);
+        GameObject taskObject = Instantiate(pieces[UnityEngine.Random.Range(0, board.colorCount - 1)]);
+        taskObject.transform.SetParent(task);
+        taskObject.transform.localScale = new Vector2(taskScale, taskScale);
+        taskObject.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
+
+        taskObject.transform.SetParent(tasks[2].transform);
+        taskObject.transform.position = new Vector2(board.width, task.transform.position.y);
+        taskObject.transform.DOMoveX(tasks[2].transform.position.x, 0.3f);
     }
 
     private void SpawnTask(Transform parent)
     {
-        GameObject taskObject = Instantiate(pieces[UnityEngine.Random.Range(0, board.colorCount)]);
+        GameObject taskObject = Instantiate(pieces[UnityEngine.Random.Range(0, board.colorCount - 1)]);
+        //Debug.Log("Random:" + UnityEngine.Random.Range(0, board.colorCount - 1));
         taskObject.transform.SetParent(parent);
         taskObject.transform.localScale = new Vector2(taskScale, taskScale);
         taskObject.transform.localPosition = parent.localPosition;
